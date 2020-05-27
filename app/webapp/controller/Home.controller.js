00162dee28ec582005c8c305094d7eadd12509db
jérémy <chabert.jeremy@gmail.com>
2020-05-27 19:17:07 +0200

    Create raw app
    EasyUI5 scaffolder

---
+++ app/webapp/controller/Home.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "com/ssg/myUI5App/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("com.ssg.myUI5App.controller.Home", {});
+});
