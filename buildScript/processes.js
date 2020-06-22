69808f01de6002eb934baeebd65767c70dc72b8d
Theo SEBASTIEN <theo.sebastien@soprasteria.com>
2020-06-22 11:39:36 +0200

    Nodes generation impl

--- buildScript/processes.js
+++ buildScript/processes.js
@@ -5,8 +5,24 @@ casual.define('group', (index) => ({
     title: `Group ${index}`,
 }));
 
-const generateNodes = (groups, max) => {
-    
+casual.define('node', (index, groups) => ({
+    key: index,
+    title: casual.title,
+    // for statuses: https://sapui5.hana.ondemand.com/#/api/sap.suite.ui.commons.networkgraph.ElementStatus:
+    status: casual.random_element(['Error', 'Information', 'Standard', 'Success', 'Warning']),
+    icon: 'sap-icon://' + casual.random_element(['factory', 'machine', 'product', 'shipping-status', 
+        'employee-approvals', 'employee-rejections', 'wrench', 'customize', 'source-code', 'laptop',
+        'it-system', 'camera', 'touch', 'accidental-leave', 'action', 'away', 'bar-code', 'batch-payements',
+        'cart', 'car-rental', 'citizen-connect', 'compare', 'delete', 'e-care']),
+    group: casual.random_element(groups).key,
+}));
+
+const generateNodes = (max, groups) => {
+    const nodes = [];
+    for (let i = 0; i < max; i++) {
+        nodes.push(casual.node(i, groups));
+    }
+    return nodes;
 };
 
 const generateLines = () => {};
@@ -18,6 +34,10 @@ const generateGroups = (max) => {
     }
     return groups;
 };
+
+// const groups = generateGroups(5);
+// const nodes = generateNodes(30, groups);
+// console.log(nodes);
 
 module.exports = {
     generateNodes,
