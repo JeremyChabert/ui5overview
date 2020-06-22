b838fd41b1bb7670fb01d87fba852f45da530422
Theo SEBASTIEN <theo.sebastien@soprasteria.com>
2020-06-22 11:02:15 +0200

    Created processes.js file to generate graph data

---
+++ buildScript/processes.js
@@ -0,0 +1,13 @@
+const casual = require('casual');
+
+const generateNodes = () => {};
+
+const generateLines = () => {};
+
+const generateGroups = () => {};
+
+module.exports = {
+    generateNodes,
+    generateLines,
+    generateGroups,
+};
