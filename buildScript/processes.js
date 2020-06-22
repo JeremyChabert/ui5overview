e5ccd8e2f601f60e5c08cf979a266edbfef82e96
Theo SEBASTIEN <theo.sebastien@soprasteria.com>
2020-06-22 11:59:02 +0200

    Lines data generation impl & changes on groups and nodes titles

--- buildScript/processes.js
+++ buildScript/processes.js
@@ -1,20 +1,27 @@
 const casual = require('casual');
 
+const capitalize = (s) => s.charAt(0).toUpperCase() + s.slice(1);
+
 casual.define('group', (index) => ({
     key: index,
-    title: `Group ${index}`,
+    title: casual.title,
 }));
 
 casual.define('node', (index, groups) => ({
     key: index,
-    title: casual.title,
-    // for statuses: https://sapui5.hana.ondemand.com/#/api/sap.suite.ui.commons.networkgraph.ElementStatus:
+    title: capitalize(casual.word),
+        // for statuses: https://sapui5.hana.ondemand.com/#/api/sap.suite.ui.commons.networkgraph.ElementStatus:
     status: casual.random_element(['Error', 'Information', 'Standard', 'Success', 'Warning']),
     icon: 'sap-icon://' + casual.random_element(['factory', 'machine', 'product', 'shipping-status', 
         'employee-approvals', 'employee-rejections', 'wrench', 'customize', 'source-code', 'laptop',
         'it-system', 'camera', 'touch', 'accidental-leave', 'action', 'away', 'bar-code', 'batch-payements',
         'cart', 'car-rental', 'citizen-connect', 'compare', 'delete', 'e-care']),
     group: casual.random_element(groups).key,
+}));
+
+casual.define('line', (index, nodes) => ({
+    from: index,
+    to: casual.random_element(nodes).key,
 }));
 
 const generateNodes = (max, groups) => {
@@ -25,7 +32,14 @@ const generateNodes = (max, groups) => {
     return nodes;
 };
 
-const generateLines = () => {};
+const generateLines = (nodes) => {
+    const lines = [];
+    const max = nodes.length;
+    for (let i = 0; i < max; i++) {
+        lines.push(casual.line(i, nodes));
+    }
+    return lines;
+};
 
 const generateGroups = (max) => {
     const groups = [];
@@ -35,8 +49,12 @@ const generateGroups = (max) => {
     return groups;
 };
 
-// const groups = generateGroups(5);
-// const nodes = generateNodes(30, groups);
+const groups = generateGroups(5);
+const nodes = generateNodes(30, groups);
+const lines = generateLines(nodes);
+console.log(groups);
+console.log(nodes);
+console.log(lines);
 // console.log(nodes);
 
 module.exports = {
